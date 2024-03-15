a72a4db38ff565160a56d8c341095e6acd513321
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-15 00:35:54 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -14,6 +14,6 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
 
 array: Tuple = (12, 72, 91)
 
-zoom_2x: List = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-zoom_3x: List = zoom_array(array, 3.0)
+zoom_3x = zoom_array(array, 3.0)
