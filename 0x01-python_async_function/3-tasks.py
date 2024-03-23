687c13346fa743b3000f452c921d66e78f30cb24
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-03-23 00:18:56 +0000

    creating a task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+  0x01. Python - Async
+  Task #3: Tasks
+"""
+import asyncio
+from typing import Coroutine
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Coroutine:
+    """Returns a asyncio.Task
+
+      Args:
+       max_delay: int
+
+      Returns:
+       a asyncio.Task
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
