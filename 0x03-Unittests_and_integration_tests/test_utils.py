f7428d674dfecc70cec41eb1847c431742c8e07e
mohamed-elghafiani <mohamed.elghafiani@ump.ac.ma>
2024-04-12 16:54:27 -0700

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,9 +3,11 @@
 """
 import unittest
 from parameterized import parameterized
+from unittest.mock import Mock, patch
 
 
 access_nested_map = __import__("utils").access_nested_map
+get_json = __import__("utils").get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,7 +28,27 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """to test that a KeyError is raised
+        """test that a KeyError is raised
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """unit tests for utils.get_json function
+    """
+    @patch('requests.get')
+    def test_get_json(self, mock_get):
+        """test that the method returns what it supposed to
+        """
+        test_cases = [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+        for (test_url, test_payload) in test_cases:
+            mock_get.return_value = Mock(json=Mock(return_value=test_payload))
+            result = get_json(test_url)
+
+            self.assertEqual(result, test_payload)
+            mock_get.assert_called_once_with(test_url)
+            mock_get.reset_mock()
